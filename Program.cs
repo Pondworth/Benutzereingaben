3c0b813a0bd439dae132cb7913af275e4acd5943
Pondworth <amy@pondworth.de>
2022-02-05 15:48:54 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benutzereingaben
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gebe bitte deinen Namen ein: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Gebe dein Alter ein: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hallo {0}, Du bist also {1} Jahre alt!", name, age);
+            //Console.WriteLine("Hallo " + name + " du bist also " + age + "Jahre alt");
+
+            //Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
